7f96fb9127d44b82388e5c86a5a8dbbc4edfde02
Suvendu Kumar Patra <suvepatra2003@gmail.com>
2025-08-11 16:17:00 +0530

    initial changes

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Rative Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/">Price</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
