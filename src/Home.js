49390d52f68128176086616dc53feae14ee5e72b
Suvendu Kumar Patra <suvepatra2003@gmail.com>
2025-08-12 01:02:21 +0530

    added a useState hook for dynamic renfering.

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const Home = () => {
 
     const handleClick = () => {
@@ -8,11 +10,28 @@ const Home = () => {
         console.log(`Hello ${name}, welcome to the Rative Blog!`);
     }
 
+    // Function for using "useState" hook
+    const [name, setName] = useState('Rative');
+    const [age, setAge] = useState(25);
+
+    const changeName = () => {
+        setName('Suvendu');
+        setAge(50);
+    }
+
     return ( 
         <div>
-            <h1>Welcome to the Rative Blog</h1>
-            <button onClick={handleClick} className="home-btn">Click Here</button>
-            <button onClick={handleNewClick('Suvendu')} className="home-btn">New Click</button>
+            <div className="container-1">
+                <h1>Welcome to the Rative Blog</h1>
+                <button onClick={handleClick} className="home-btn">Click Here</button>
+                <button onClick={handleNewClick('Suvendu')} className="home-btn">New Click</button>
+                
+                {/* Using the 'useState' hook for changing the name after clicking the button. */}
+                <button onClick={changeName} className="home-btn">Change Details</button>
+            </div>
+            <div className="container-2">
+                <p>{name} is {age} years old!</p>
+            </div>
         </div>
     );
 }
