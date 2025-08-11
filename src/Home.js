309191e7161727f67ce29f912894c7caca7aac91
Suvendu Kumar Patra <suvepatra2003@gmail.com>
2025-08-11 17:55:45 +0530

    some changes added in Home component with explanations

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,23 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('Check Mate!');
+    };
+
     return ( 
         <div>
             <h1>Welcome to the Rative Blog</h1>
+            <button onClick={handleClick}>Click...</button>
         </div>
     );
 }
     
 export default Home;
+
+/**
+ * This is a simple Home component that displays a welcome message and a button.
+ * When the button is clicked, it logs a message to the console.
+ * 
+ * >> When I used the dynamic value in OnClick event as 'handleClick' then it would display the message in console after clicking the button.
+ * >> If I used the static value 'handleClick()' then it would display the message in console immediately after rendering the component.
+ */
