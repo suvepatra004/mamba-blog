82e23ec9eb98716b6578eb72b2d976c4c59519ca
Suvendu Kumar Patra <suvepatra2003@gmail.com>
2025-08-17 12:59:49 +0530

    Uses the Route method after importing the react-router-dom library, used for all the blogs components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
 import SecondaryHome from "./SecondaryHome";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <Navbar />
-        <div className="content">
-          <Home />
-          <SecondaryHome />
-        </div>
+      <Navbar />
+      <div className="content">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/secondary" element={<SecondaryHome />} />
+          </Routes>
+        </Router>
       </div>
     </div>
   );
