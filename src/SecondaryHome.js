3aae482490a6be9632d777e7b627e97a16bdeb0f
Suvendu Kumar Patra <suvepatra2003@gmail.com>
2025-08-17 11:46:54 +0530

    Added a error handling div by adding a useState for errors

--- src/SecondaryHome.js
+++ src/SecondaryHome.js
@@ -4,23 +4,33 @@ import { useState, useEffect } from "react";
 const SecondaryHome = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     // npx json-server --watch data/ds.json --port 8010 => This command starts a JSON server to serve the data from ds.json file
     setTimeout(() => {
       fetch("http://localhost:8010/blogs")
         .then((res) => {
+          if (!res.ok) {
+            throw Error("!! Could not fetch the data from the server");
+          }
           return res.json();
         })
         .then((data) => {
           setBlogs(data);
           setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
         });
     }, 1000); // Simulating a delay of 1 second before fetching data
   }, []);
 
   return (
     <div className="home container">
+      {error && <div className="error-message">{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <Blogs blogs={blogs} title="Newly Added Blogs" />}
     </div>
