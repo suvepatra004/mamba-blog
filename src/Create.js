486207b9eccae14136cff352a565586a52120288
Suvendu Kumar Patra <suvepatra2003@gmail.com>
2025-08-21 21:52:33 +0530

    added a Create.js for adding new blogs in the Blog List and added lil bit css for the Create Blog Form

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,26 @@ const Create = () => {
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form action="">
+        {/* Form fields for creating a new blog */}
+        <label for="title">Blog Title:</label>
+        <input name="title" id="create-blog-title" type="text" required />
+
+        {/* Textarea for blog body */}
+        <label for="body">Blog body: </label>
+        <textarea name="body" id="create-blog-body" required></textarea>
+
+        {/* Input for blog author */}
+        <label for="select">Blog Author:</label>
+        <select name="select" id="select-author">
+          <option value="default">Select Author</option>
+          <option value="Mario">Mario</option>
+          <option value="Yoshi">Yoshi</option>
+        </select>
+
+        {/* Submit button for the form */}
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
